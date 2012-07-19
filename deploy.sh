7928f280236f9caa3dff4cebb9262c45f9eae432
Andrey Chernih <andrey.chernih@gmail.com>
2012-07-19 15:43:45 +0400

    chef-solo deployment files

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tar cj . | ssh root@$HOST "
+((test -z `which bzip2` && apt-get -y install bzip2) || true) &&
+rm -rf ~/chef &&
+mkdir ~/chef &&
+(mkdir ~/chef-cache > /dev/null 2>&1 || true) &&
+cd ~/chef &&
+tar jx &&
+HOST=$HOST ROLE=$ROLE ./install.sh"
