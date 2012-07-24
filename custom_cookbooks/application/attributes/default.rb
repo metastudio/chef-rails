878e04cc966bd87075c291cc75178bbcaf005d91
Andrey Chernih <andrey.chernih@gmail.com>
2012-07-24 15:06:14 +0400

    configure unicorn

--- custom_cookbooks/application/attributes/default.rb
+++ custom_cookbooks/application/attributes/default.rb
@@ -1,3 +1,4 @@
+default[:application][:current_path]  = "#{node[:application][:path]}/current"
 default[:application][:shared_path]   = "#{node[:application][:path]}/shared"
 default[:application][:config_path]   = "#{node[:application][:shared_path]}/config"
 default[:application][:releases_path] = "#{node[:application][:path]}/releases"
@@ -6,3 +7,6 @@ default[:application][:user][:home]   = "/home/#{node[:application][:user][:name
 
 default[:database][:name]             = "#{node[:application][:name]}_#{node[:application][:environment]}"
 default[:database][:user]             = node[:application][:user][:name]
+
+default[:application][:workers]       = 2
+default[:application][:socket]        = "/tmp/#{node[:application][:name]}.socket"
--- custom_cookbooks/application/recipes/config.rb
+++ custom_cookbooks/application/recipes/config.rb
@@ -8,3 +8,9 @@ template "#{node[:application][:config_path]}/database.yml" do
   owner node[:application][:user][:name]
   group node[:application][:user][:name]
 end
+
+template "#{node[:application][:config_path]}/unicorn.rb" do
+  source "config/unicorn.rb.erb"
+  owner node[:application][:user][:name]
+  group node[:application][:user][:name]
+end
