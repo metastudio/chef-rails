ce894a2206509b8b27dccd66259929ffb68b16ad
Andrey Chernih <andrey.chernih@gmail.com>
2012-07-24 14:51:31 +0400

    generate database config

--- custom_cookbooks/application/attributes/default.rb
+++ custom_cookbooks/application/attributes/default.rb
@@ -1,4 +1,8 @@
 default[:application][:shared_path]   = "#{node[:application][:path]}/shared"
+default[:application][:config_path]   = "#{node[:application][:shared_path]}/config"
 default[:application][:releases_path] = "#{node[:application][:path]}/releases"
 
 default[:application][:user][:home]   = "/home/#{node[:application][:user][:name]}"
+
+default[:database][:name]             = "#{node[:application][:name]}_#{node[:application][:environment]}"
+default[:database][:user]             = node[:application][:user][:name]
---
+++ custom_cookbooks/application/recipes/config.rb
@@ -0,0 +1,10 @@
+directory node[:application][:config_path] do
+  owner node[:application][:user][:name]
+  group node[:application][:user][:name]
+end
+
+template "#{node[:application][:config_path]}/database.yml" do
+  source "config/database.yml.erb"
+  owner node[:application][:user][:name]
+  group node[:application][:user][:name]
+end
