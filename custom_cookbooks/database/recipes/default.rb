0babf5753ea9e0a178a4087d31ec08f42ecf6450
Andrey Chernih <andrey.chernih@gmail.com>
2012-08-10 18:10:03 +0400

    Enable nginx site

--- custom_cookbooks/database/recipes/default.rb
+++
@@ -1,1 +0,0 @@
-package "mysql-server-5.5"
--- custom_cookbooks/frontend/recipes/default.rb
+++ custom_cookbooks/frontend/recipes/default.rb
@@ -1,5 +1,8 @@
 include_recipe "nginx::source"
 
-template "#{node[:nginx][:dir]}/sites-enabled/#{node[:application][:name]}.conf" do
+conf_name = "#{node[:application][:name]}.conf"
+
+template "#{node[:nginx][:dir]}/sites-enabled/#{conf_name}" do
   source "nginx.site.conf.erb"
 end
+nginx_site conf_name
