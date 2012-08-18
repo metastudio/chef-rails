5e132cb2ed1301e712097e23870f47ad058fa087
Andrey Chernih <andrey.chernih@gmail.com>
2012-08-18 21:45:41 +0400

    Looks like we need this for mysql gem

--- custom_cookbooks/application/recipes/software.rb
+++ custom_cookbooks/application/recipes/software.rb
@@ -1,1 +1,1 @@
-%w(build-essential git git-core htop atop screen imagemagick vim).map{|p| package p}
+%w(build-essential git git-core htop atop screen imagemagick vim libmysqlclient-dev).map{|p| package p}
