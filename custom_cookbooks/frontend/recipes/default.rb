1697b7f154662d088cd56b333ff701534af23ab4
Andrey Chernih <andrey.chernih@gmail.com>
2012-09-05 17:45:09 +0400

    Add SSL support

--- custom_cookbooks/frontend/recipes/default.rb
+++ custom_cookbooks/frontend/recipes/default.rb
@@ -2,8 +2,15 @@ include_recipe "nginx::source"
 
 conf_name = "#{node[:application][:name]}.conf"
 
-template "#{node[:nginx][:dir]}/sites-enabled/#{conf_name}" do
+# main site config which goes to the sites-available/ dir
+template "#{node[:nginx][:dir]}/sites-available/#{conf_name}" do
   source "nginx.site.conf.erb"
+end
+
+# include file which included in main site config 
+# (in order to use same configuration for multiple servers: http and https for example)
+template "#{node[:nginx][:dir]}/sites-available/conf.d/#{node[:application][:name]}.include.conf" do
+  source "nginx.site.include.conf.erb"
 end
 
 nginx_site conf_name
