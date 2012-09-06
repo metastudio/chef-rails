45cea069e8cc11318c180038e9ba50efb8f96b4e
Andrey Chernih <andrey.chernih@gmail.com>
2012-09-06 19:23:41 +0400

    Reload nginx when template changes

--- custom_cookbooks/frontend/recipes/default.rb
+++ custom_cookbooks/frontend/recipes/default.rb
@@ -5,12 +5,14 @@ conf_name = "#{node[:application][:name]}.conf"
 # main site config which goes to the sites-available/ dir
 template "#{node[:nginx][:dir]}/sites-available/#{conf_name}" do
   source "nginx.site.conf.erb"
+  notifies :reload, resources(:service => "nginx")
 end
 
 # include file which included in main site config 
 # (in order to use same configuration for multiple servers: http and https for example)
 template "#{node[:nginx][:dir]}/sites-available/conf.d/#{node[:application][:name]}.include.conf" do
   source "nginx.site.include.conf.erb"
+  notifies :reload, resources(:service => "nginx")
 end
 
 nginx_site conf_name
