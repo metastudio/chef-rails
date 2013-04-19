d5da3246d4e0e319c856ac58ccd75cec7cb8ad69
Andrey Chernih <andrey.chernih@gmail.com>
2013-04-19 15:34:12 +0400

    Move nginx configuration to the JSON config

--- custom_cookbooks/frontend/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-set[:nginx][:version]         = "1.2.8"
-set[:nginx][:source][:prefix] = "/usr/local/nginx"
-set[:nginx][:dir]             = "/usr/local/nginx/conf"
-set[:nginx][:log_dir]         = "/usr/local/nginx/log"
-set[:nginx][:binary]          = "/usr/local/nginx/sbin/nginx"
-
-set[:nginx][:user]            = "www-data"
-set[:nginx][:init_style]      = "init"
