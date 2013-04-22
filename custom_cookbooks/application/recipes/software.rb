6ee6ad21d7b6928f7f03ad9fefcca7acd29d745e
Andrey Chernih <andrey.chernih@gmail.com>
2013-04-22 16:40:30 +0400

    We need mailutils

--- custom_cookbooks/application/recipes/software.rb
+++ custom_cookbooks/application/recipes/software.rb
@@ -1,1 +1,1 @@
-%w(build-essential git git-core htop atop screen imagemagick vim libmysqlclient-dev openjdk-7-jre).map{|p| package p}
+%w(build-essential git git-core htop atop screen imagemagick vim libmysqlclient-dev openjdk-7-jre mailutils).map{|p| package p}
